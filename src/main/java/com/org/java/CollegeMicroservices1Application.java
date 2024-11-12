0cdba5e3c564fc9ed48e8ca5db4bea81ee891a93
Thrishank99 <thrishank966@gmail.com>
2024-11-12 21:25:48 +0530

    fwedasxz

--- src/main/java/com/org/java/CollegeMicroservices1Application.java
+++ src/main/java/com/org/java/CollegeMicroservices1Application.java
@@ -2,12 +2,18 @@ package com.org.java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class CollegeMicroservices1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CollegeMicroservices1Application.class, args);
+	}
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
