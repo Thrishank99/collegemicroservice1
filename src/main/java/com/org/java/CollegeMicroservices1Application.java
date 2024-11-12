751be618f7190c7c1d3617bbb9cec518b25651e1
Thrishank99 <thrishank966@gmail.com>
2024-11-12 21:14:15 +0530

    first commit

---
+++ src/main/java/com/org/java/CollegeMicroservices1Application.java
@@ -0,0 +1,13 @@
+package com.org.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeMicroservices1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeMicroservices1Application.class, args);
+	}
+
+}
