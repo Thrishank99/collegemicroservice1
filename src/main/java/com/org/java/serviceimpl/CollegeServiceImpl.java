765f2f310d07c90df11b2efa69f1353c8d3f1dee
Thrishank99 <thrishank966@gmail.com>
2024-11-13 10:50:17 +0530

    fSDZ

--- src/main/java/com/org/java/serviceimpl/CollegeServiceImpl.java
+++ src/main/java/com/org/java/serviceimpl/CollegeServiceImpl.java
@@ -41,7 +41,7 @@ public class CollegeServiceImpl implements CollegeService {
 	@Override
 	public Object[] getCollegeToStudentDetails() {
 	//Object[] students=restTemplate.getForObject(ENDPOINT_URL, Object[].class);
-	Object[] students=restTemplate.getForObject("http://localhost:2023/student/findAll", Object[].class);
+	Object[] students=restTemplate.getForObject("http://localhost:8082/college/findAll", Object[].class);
 		return students;
 	}
 
